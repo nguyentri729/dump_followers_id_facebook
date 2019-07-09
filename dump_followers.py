6e0a741f60df8b4f8a1dbc89a6630bbd59ed3ede
nguyentri729 <nguyentri729.2018@gmail.com>
2019-07-09 22:13:49 +0700

    inital commit

---
+++ dump_followers.py
@@ -0,0 +1,20 @@
+import requests, json
+access_token = 'access token here'
+stop = False
+url = 'https://graph.facebook.com/me/subscribers?method=get&limit=100&access_token='+access_token;
+f = open("list_uid.txt","w+")
+while stop == False:
+	r = requests.get(url);
+	try:
+		result = json.loads(r.text)
+		for fl in result['data']:
+			print(f.write(fl['id']+'\n'))
+		if result['paging']['next'] != '':
+			#code
+			print(result['paging']['next'])
+			url = result['paging']['next']
+		else:
+			stop = True
+	except Exception as e:
+		stop = True
+	
